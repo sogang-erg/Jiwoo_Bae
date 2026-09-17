fa1dd2182b98e096b2f797c83046527d8bce2618
jiwoo-bae <121778994+jiwoo-bae@users.noreply.github.com>
2026-09-17 10:56:27 +0900

    섹션 6

--- Week2/Assets/Script/Player.cs
+++ Week2/Assets/Script/Player.cs
@@ -1,17 +1,36 @@
 using UnityEngine;
 
 public class Player : Character
-
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f;
+   
     void Start()
     {
-        
+  
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 dir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            dir += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            dir += Vector3.right;
+        }
+
+        transform.position += dir.normalized * speed * Time.deltaTime;
     }
 }
+
