b0a86db636ca98cd87351ce4358b574f36b206ff
jiwoo-bae <121778994+jiwoo-bae@users.noreply.github.com>
2026-09-22 18:40:29 +0900

    섹션7

--- Week2/Assets/Script/Player.cs
+++ Week2/Assets/Script/Player.cs
@@ -1,36 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : Character
 {
-    public float speed = 5f;
    
+    private InputAction _testAction;
     void Start()
     {
-  
+        
+        _testAction = InputSystem.actions.FindAction("Player/Test");
+
+        _testAction.Enable();
+
     }
 
-    void Update()
+      void Update()
     {
-        Vector3 dir = Vector3.zero;
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            dir += Vector3.forward;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            dir += Vector3.left;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            dir += Vector3.back;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            dir += Vector3.right;
-        }
-
-        transform.position += dir.normalized * speed * Time.deltaTime;
+        Vector2 move = _testAction.ReadValue<Vector2>();
+        Debug.Log("Move: " + move  );
     }
 }
 
